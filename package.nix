2921057a6f6c11ae4550cb60e1f789da949592d2
algorithmiker <104317939+algorithmiker@users.noreply.github.com>
2025-09-02 21:37:41 +0200

    nix: fix build

--- package.nix
+++ package.nix
@@ -1,17 +1,43 @@
 {
   lib,
+  breakpointHook,
+  libGL,
+  libxkbcommon,
   luajit,
+  mold-wrapped,
+  patchelf,
   pkg-config,
   rustPlatform,
+  wayland,
 }:
+let
+  extraRPATHLibs = [
+    libGL
+    libxkbcommon
+    wayland
+  ];
+in
 rustPlatform.buildRustPackage {
   pname = "entrace";
   version = "0.1.0";
   buildInputs = [
+    libxkbcommon
     luajit
+    wayland
   ];
-  nativeBuildInputs = [ pkg-config ];
+  nativeBuildInputs = [
+    breakpointHook
+    mold-wrapped
+    patchelf
+    pkg-config
+  ];
   src = lib.cleanSource ./.;
+  postFixup = ''
+    ENTRACE_BIN="$out/bin/entrace"
+    patchelf --add-rpath ${lib.makeLibraryPath extraRPATHLibs} "$ENTRACE_BIN"
+    patchelf --add-needed libwayland-client.so --add-needed libxkbcommon.so --add-needed libEGL.so "$ENTRACE_BIN"
+  '';
+  env.RUSTFLAGS = "-C link-arg=-fuse-ld=mold";
   cargoHash = "sha256-3yLmsBJbptRBMkpB1Odt6WzIlCmMAgOVn03Ho6gzFO8=";
   meta = {
     mainProgram = "entrace";
