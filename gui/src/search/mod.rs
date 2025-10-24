0ca3db55bde3c31cf7574a39b3bd923ef5b4e99a
algorithmiker <104317939+algorithmiker@users.noreply.github.com>
2025-10-24 21:25:44 +0200

    gui: misc

--- gui/src/search/mod.rs
+++ gui/src/search/mod.rs
@@ -59,23 +59,7 @@ impl QueryTiming {
         }
     }
 }
-#[derive(thiserror::Error, Debug, Clone)]
-pub enum QueryError {
-    #[error("Index out of bounds. Tried to access element {index} of a container of size {actual}")]
-    OutOfBounds { index: u32, actual: u32 },
-    #[error(
-        "The thread running your query died. This usually means an error in entrace, and not in \
-         your code."
-    )]
-    QueryDied,
-    #[error("Error while running your query")]
-    LuaError(#[source] mlua::Error),
-    #[error(
-        "Failed to coerce the result of your query to Vec<u32>. Make sure to return a list from \
-         the query!"
-    )]
-    FailedToCoerce(#[source] mlua::Error),
-}
+
 enum QuerySettingsDialogData {
     Closed,
     Open { settings_button_rect: Rect, position: Option<Pos2> },
@@ -162,7 +146,7 @@ impl SearchState {
                         let start = Instant::now();
                         let loaded: Result<Value, _> =
                             lua.load(&*ta).set_name("search query").eval();
-                        info!("Thread {i} took {:?}", start.elapsed());
+                        info!(elapsed = ?start.elapsed(), "Thread {i} done");
                         match loaded {
                             Ok(x) => {
                                 let ids: Result<_, _> = Vec::from_lua(x, &lua)
