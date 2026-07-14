fe5a5a819e5cd719e25744e74544f0d160c36a49
algorithmiker <104317939+algorithmiker@users.noreply.github.com>
2026-07-14 12:31:52 +0200

    nix: update deps, fix dioxus-cli

--- shell.nix
+++ shell.nix
@@ -4,6 +4,15 @@
   rustfmt-nightly ? pkgs.rustfmt,
 }:
 let
+  # https://github.com/DioxusLabs/dioxus/issues/5540#issuecomment-4433970098
+  # needed for dioxus-cli 0.7.5 with working hot reload
+  nixpkgsOld = import (pkgs.fetchFromGitHub {
+    owner = "nixos";
+    repo = "nixpkgs";
+    rev = "01fbdeef22b76df85ea168fbfe1bfd9e63681b30";
+    hash = "sha256-GMSVw35Q+294GlrTUKlx087E31z7KurReQ1YHSKp5iw=";
+  }) { system = pkgs.system; };
+
   libPathPackages = [
     pkgs.wayland
     pkgs.libxkbcommon
@@ -20,7 +29,7 @@ pkgs.mkShell {
     pkgs.zenity
     rustfmt-nightly
     pkgs.cargo-about
-    pkgs.dioxus-cli
+    nixpkgsOld.dioxus-cli
     pkgs.cargo-semver-checks
   ];
   preferLocalBuild = true;
